99451a210bf774190ab50ce78f49a35adf25aa13
egboyguda <e.gboyguda@gmail.com>
2020-02-24 07:59:05 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+
+var kalaban = supervillains.random();
+var hero = superheroes.random();
+console.log(hero);
+console.log(kalaban);
